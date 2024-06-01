2feb0a9044136ea253658256065c40fc6979a843
Jim Rhotert <github@rhotertj.de>
2024-06-01 14:19:29 +0200

    remove placeholder value for last update

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,11 @@
-export function Footer() {
+import { fetchLastUpdate } from "../lib/metadata";
+
+export async function Footer() {
+    const lastUpdate : string = await fetchLastUpdate();
+    console.log(`last update ${lastUpdate}`)
     return (
         <div>
-            <div className="flex justify-center">Letzter Stand: 12.02.2024</div>
+            <div className="flex justify-center">Letzter Stand: {new Date(lastUpdate).toLocaleString("de-DE", {dateStyle: "long", timeStyle: "short"})} Uhr</div>
         </div>
     )
 }
--- lib/metadata.ts
+++ lib/metadata.ts
@@ -1,21 +1,30 @@
 import prisma from "@/lib/database";
 
 export type metaDataType = {
-    players: number;
-    matches: number;
-    competitions: number;
+  players: number;
+  matches: number;
+  competitions: number;
+};
+
+export async function fetchMetaData() {
+  const nPlayers = await prisma.human.count();
+  const nSingles = await prisma.singlesmatch.count();
+  const nDoubles = await prisma.doublesmatch.count();
+  const nCompetitions = await prisma.competition.count();
+  await Promise.all([nPlayers, nDoubles, nSingles, nCompetitions]);
+  const data = {
+    players: nPlayers,
+    matches: nSingles + nDoubles,
+    competitions: nCompetitions,
   };
-  
-  export async function fetchMetaData() {
-    const nPlayers = await prisma.human.count();
-    const nSingles = await prisma.singlesmatch.count();
-    const nDoubles = await prisma.doublesmatch.count();
-    const nCompetitions = await prisma.competition.count();
-    await Promise.all([nPlayers, nDoubles, nSingles, nCompetitions]);
-    const data = {
-      players: nPlayers,
-      matches: nSingles + nDoubles,
-      competitions: nCompetitions,
-    };
-    return data;
-  }
+  return data;
+}
+
+export async function fetchLastUpdate() {
+  const lastUpdate = await prisma.skillrating.aggregate({
+    _max: {
+      latest_update: true,
+    },
+  });
+  return lastUpdate._max.latest_update;
+}
